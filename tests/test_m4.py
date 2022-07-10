baaf4314c467b8d4ef2a41a470c87580cf12a31c
Alex Drozd <drozdster@gmail.com>
2022-07-10 16:13:09 -0500

    setup before mocking

---
+++ tests/test_m4.py
@@ -0,0 +1,17 @@
+from P4 import P4, P4Exception  # Import the module
+
+from m4python import mock_p4
+
+
+@mock_p4
+def test_m4():
+    p4 = P4()  # Create the P4 instance
+    p4.port = "1666"
+    p4.user = "fred"
+    p4.client = "fred-ws"  # Set some environment variables
+
+    try:  # Catch exceptions with try/except
+        p4.connect()  # Connect to the Perforce server
+    except P4Exception as reason:
+        print(reason)
+        assert False
