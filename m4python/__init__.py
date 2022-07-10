7ed58d0ab2e8cae295f8ebb819b50c4eeb082192
Alex Drozd <drozdster@gmail.com>
2022-07-10 17:03:46 -0500

    mocking catches imports

--- m4python/__init__.py
+++ m4python/__init__.py
@@ -4,11 +4,13 @@ from decorator import decorator
 
 from m4python.m4 import M4
 
+mocked_p4 = patch("P4.P4", M4)
+# start the mock outside the decorator so we catch imports before it's applied
+mocked_p4.start()
+
 
 @decorator
 def mock_p4(func, *args, **kwargs):
-    mocked_p4 = patch("P4.P4", M4)
-    mocked_p4.start()
     called = func(*args, **kwargs)
     mocked_p4.stop()
     return called
